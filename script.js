cae5758e15e1493a0d2597666cd2f15d8c58b887
aminosbk6-code <aminosbk6@gmail.com>
2026-01-26 20:16:03 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,8 @@
-
-
-// simple animation on scroll
 window.addEventListener("scroll", () => {
-  const cards = document.querySelectorAll(".card");
-  cards.forEach(card => {
-    const pos = card.getBoundingClientRect().top;
-    if (pos < window.innerHeight - 50) {
+  document.querySelectorAll(".card").forEach(card => {
+    if (card.getBoundingClientRect().top < window.innerHeight - 80) {
       card.style.opacity = 1;
+      card.style.transform = "translateY(0)";
     }
   });
 });
